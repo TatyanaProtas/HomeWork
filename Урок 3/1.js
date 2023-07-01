2adddf67dbf0dbda7a967360894ee65503a6f0bc
TatyanaProtas <tatyana.protas.89@gmail.com>
2023-07-01 14:24:40 +0300

    урок 3

---
+++ Урок 3/1.js
@@ -0,0 +1,6 @@
+const num = Number.parseFloat(prompt("Введите температуру в градусах по Фаренгейту:"));
+printTemperature(num);
+
+function printTemperature(num) {
+    alert(`Цельсий: ${((num - 32)/1.8).toFixed(0)}, Фаренгейт: ${num}.`);
+}
